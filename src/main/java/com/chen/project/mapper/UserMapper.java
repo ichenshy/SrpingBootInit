70d561fdb25b6a6637845b0895b409dac8753890
glalaxy <galaxy_ac@163.com>
2022-08-09 23:26:19 +0800

    commit init

---
+++ src/main/java/com/chen/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.chen.project.mapper;
+
+import com.chen.project.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * UserMapper
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
